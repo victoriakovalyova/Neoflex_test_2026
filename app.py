f355b5d2b158f6c3f4d6057fffa85b42ae6ca471
victoriakovalyova <74002628+victoriakovalyova@users.noreply.github.com>
2026-09-17 01:42:36 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from inference import classify
+
+app = FastAPI()
+
+@app.post("/classify")
+def classify_endpoint(text: str):
+    return classify(text)
+
+# Запуск: uvicorn app:app --host 0.0.0.0 --port 8000
